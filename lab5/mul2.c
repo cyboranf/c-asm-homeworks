a04b2747114cfac70af747f9f51ea8d4d93ff536
cyboranf <cyboranf@gmail.com>
2023-04-19 17:43:53 +0200

    ex 5.4 -done

--- lab5/mul2.c
+++ lab5/mul2.c
@@ -1,3 +1,18 @@
-//
-// Created by Filip on 19.04.2023.
-//
+#include <stdio.h>
+
+int main() {
+    printf("mul2.c\n\n");
+
+    unsigned int a = 4294967295;
+    unsigned int b = 2;
+
+    unsigned long long result =(unsigned long long ) a * b;
+
+    printf("a = %u\n", a);
+    printf("b = %u\n\n", b);
+
+    printf("iloczyn = %lld", result);
+
+    return 0;
+}
+
