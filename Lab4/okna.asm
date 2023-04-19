6ffd664b9032cb68be21f04f90dd4729ea974c5b
cyboranf <cyboranf@gmail.com>
2023-04-19 18:34:47 +0200

    ex 4.11 - done

--- Lab4/okna.asm
+++ Lab4/okna.asm
@@ -1,48 +1,107 @@
-         [bits 32]
+        [bits 32]
 
 ;        esp -> [ret]  ; ret - adres powrotu do asmloader
 
-x        equ 6
 a        equ 5
 b        equ 19
+
 c        equ 12
 d        equ 24
 
-         mov eax, x    ; eax = x
-         mov edx, a    ; edx = a
-         mov ecx, b    ; ecx = b
+x        equ 25
 
-         push ecx  ; ecx -> stack
-         push edx  ; edx -> stack
+         mov eax, x    ; eax = x
+
+         cmp eax, b    ; eax - b          ; OF SF ZF AF PF CF affected
+         jg nieNalezy1  ; jump if greater  ; jump if SF == OF and ZF = 0
+
+         cmp eax, a    ; eax - a          ; OF SF ZF AF PF CF affected
+         jl nieNalezy1  ; jump if less     ; jump if SF != OF1
+
+         jmp sprawdz2
+
+nieNalezy1:
+         cmp eax, d    ; eax - b          ; OF SF ZF AF PF CF affected
+         jg nieNalezyDoObydwu  ; jump if greater  ; jump if SF == OF and ZF = 0
+
+         cmp eax, c    ; eax - a          ; OF SF ZF AF PF CF affected
+         jl nieNalezyDoObydwu  ; jump if less     ; jump if SF != OF1s
+
+         push d    ; b -> stack
+         push c    ; a -> stack
+         push eax  ; eax -> stack
+         push b    ; b -> stack
+         push a    ; a -> stack
+         push eax  ; eax -> stack
+;        esp -> [b][a][eax][ret]
+
+         call getaddr ; push on the stack the runtime address of format and jump to getaddr
+format:
+         db '%d nalezy do  (%d,%d)', 0xA,
+         db '%d nie nalezy do  (%d,%d)', 0xA, 0
+
+sprawdz2:
+         cmp eax, d    ; eax - b          ; OF SF ZF AF PF CF affected
+         jg nieNalezyDoObydwu  ; jump if greater  ; jump if SF == OF and ZF = 0
+
+         cmp eax, c    ; eax - a          ; OF SF ZF AF PF CF affected
+         jl nieNalezyDoObydwu  ; jump if less     ; jump if SF != OF1
+
+         jmp nalezyDoObu
+
+nieNalezy2:
+         cmp eax, b    ; eax - b          ; OF SF ZF AF PF CF affected
+         jg nieNalezy1  ; jump if greater  ; jump if SF == OF and ZF = 0
+
+         cmp eax, a    ; eax - a          ; OF SF ZF AF PF CF affected
+         jl nieNalezy1  ; jump if less     ; jump if SF != OF1
+
+         push d    ; b -> stack
+         push c    ; a -> stack
+         push eax  ; eax -> stack
+         push b    ; b -> stack
+         push a    ; a -> stack
          push eax  ; eax -> stack
 
-;        esp -> [eax][edx][ecx][ret]
+;        esp -> [b][a][eax][ret]
 
-         cmp eax, b     ; eax - b                  ; OF SF ZF AF PF CF affected
-         jg nienalezy  ; jump if greater ; jump if SF == OF or ZF = 1
-         cmp eax, a  ; eax - a
-         jl nienalezy  ; jump if lesser
-
-         call getaddr2 ; push on the stack the runtime address of format and jump to getaddr2
-format:
-         db '%d nalezy do [%d, %d]', 0xA, 0
-
-nienalezy:
-          call getaddr2 ; push on the stack the runtime address of format and jump to getaddr2
+         call getaddr ; push on the stack the runtime address of format and jump to getaddr
 format2:
-          db '%d nie nalezy do [%d, %d]', 0xA, 0
+         db '%d nalezy do  (%d,%d)', 0xA,
+         db '%d nie nalezy do  (%d,%d)', 0xA, 0
 
-getaddr2:
+nalezyDoObu:
+        push d    ; b -> stack
+        push c    ; a -> stack
+        push b    ; b -> stack
+        push a    ; a -> stack
+        push eax  ; eax -> stack
+;       esp -> [b][a][eax][ret]
 
-;        esp -> [format1/2][eax][edx][ecx][ret]
+        call getaddr
+format3:
+         db '%d nalezy do (%d,%d) i (%d,%d)', 0xA, 0
+         
+nieNalezyDoObydwu:
+        push d    ; b -> stack
+        push c    ; a -> stack
+        push b    ; b -> stack
+        push a    ; a -> stack
+        push eax  ; eax -> stack
+;       esp -> [b][a][eax][ret]
 
-         call [ebx+3*4]  ; printf(format, eax, edx, ecx);
-         add esp, 4*4    ; esp = esp + 8
+        call getaddr
+format4:
+         db '%d nie nalezy do (%d,%d) i (%d,%d)', 0xA, 0
+
+getaddr:
+
+;        esp -> [format][eax][edx][ret]
+
+         call [ebx+3*4]  ; printf(format, eax, edx);
+         add esp, 3*4    ; esp = esp + 12
 
 ;        esp -> [ret]
-
-         mov edx, a    ; edx = a
-         mov ecx, b    ; ecx = b
 
          push 0          ; esp -> [0][ret]
          call [ebx+0*4]  ; exit(0);
